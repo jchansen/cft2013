a51460ef0809950eb8ec842855e0d30a98362539
Jason Hansen <jason_hansen@outlook.com>
2013-07-28 11:52:23 -0700

    changing text limit on stories to 10,000 characters

--- db/migrate/20130728164911_add_teacher_information_to_users.rb
+++ db/migrate/20130728164911_add_teacher_information_to_users.rb
@@ -4,6 +4,6 @@ class AddTeacherInformationToUsers < ActiveRecord::Migration
     add_column :users, :year_of_experience, :string
     add_column :users, :credentials, :string
     add_column :users, :grade_level, :string
-    add_column :users, :story, :string
+    add_column :users, :story, :string, :length => 10000
   end
 end
